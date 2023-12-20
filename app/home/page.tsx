b754632f515b8d8f56267c1b65cb39a82b31220a
Jiang Li <lijiang0327@gmail.com>
2023-12-20 21:17:49 +0800

    fix: performance

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,13 @@
-import {getList, getRandomList} from '@/utils/getData';
+import {getList} from '@/utils/getData';
 import TopBar from './_topBar';
 import ItemList from './_itemList';
 
 const Home = async () => {
-  const [data, randomData] = await Promise.all([getList(1, 10), getRandomList()])
+  const [data] = await Promise.all([getList(1, 10)])
  
   return <div className="max-w-2xl ml-auto mr-auto pt-10">
     <h1 className="font-bold text-3xl text-cyan-500 mb-8">Beam</h1>
-    <TopBar data={randomData} />
+    <TopBar data={[data[0], data[1]]} />
     <ItemList data={data} />
   </div>
 }
